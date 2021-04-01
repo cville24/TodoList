2593397b55a22235156ce2e326e33dbe10ab3381
cville24 <cyrilvillette37@gmail.com>
2021-04-01 14:36:51 +0200

    premier commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,39 @@
 <template>
-  <div id="app">
+  <div id="app-2">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="msg2"/>
+    <InputList @event="getTask"/>
+    {{ task }}
+    <TableToDo />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import InputList from './components/InputList.vue'
+import TableToDo from './components/TableToDo'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    InputList,
+    TableToDo
+  },
+  data: ()=>({
+    msg2: "Todo List",
+    task: null
+  }),
+  methods: {
+    getTask: function(value){
+      this.task = value;
+      }
   }
 }
 </script>
 
 <style>
-#app {
+#app-2 {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/TableToDo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <table>
+        <thead>
+        <tr>
+            <th colspan="4">TodoList</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Nom de la tache</td>
+            <td>Avancement</td>
+            <td>Avancer la tache</td>
+            <td>Supprimer</td>
+        </tr>
+    </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TableToDo"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table,
+td {
+    border: 1px solid #333;
+}
+
+thead,
+tfoot {
+    background-color: #333;
+    color: #fff;
+}
+</style>
